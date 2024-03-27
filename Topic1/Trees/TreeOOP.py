c41edba71737127a6ddd850e34dccaa1a461fc77
Kipperish <152880818+Kipperish@users.noreply.github.com>
2024-03-27 10:53:03 +0000

    Tree oop traversal

--- Topic1/Trees/TreeOOP.py
+++ Topic1/Trees/TreeOOP.py
@@ -1,1 +1,72 @@
 class Node():
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+class tree():
+    def __init__(self):
+        self.root = None
+
+    def isEmpty(self):
+        if self.root == None:
+            return True
+        else:
+            return False
+
+    def addNode(self, data):
+        newNode = Node(data)
+        current = self.root
+        positionFound = False
+        if self.isEmpty():
+            self.root = newNode
+        else:
+            while not positionFound:
+                if newNode.data >= current.data:
+                    if current.right == None:
+                        current.right = newNode
+                        positionFound = True
+                    else:
+                        current = current.right
+                else:
+                    if current.left == None:
+                        current.left = newNode
+                        positionFound = True
+                    else:
+                        current = current.left
+
+    def inOrderTraverse(self):
+        if self.isEmpty():
+            print("Tree is empty")
+        else:
+            def traverse(current):
+                if current.left != None:
+                    traverse(current.left)
+                print(current.data)
+                if current.right != None:
+                    traverse(current.right)
+            traverse(self.root)
+
+    def postOrderTraverse(self):
+        if self.isEmpty():
+            print("Tree is empty")
+        else:
+            def traverse(current):
+                if current.left != None:
+                    traverse(current.left)
+                if current.right != None:
+                    traverse(current.right)
+                print(current.data)
+            traverse(self.root)
+
+    def preOrderTraverse(self):
+        if self.isEmpty():
+            print("Tree is empty")
+        else:
+            def traverse(current):
+                print(current.data)
+                if current.left != None:
+                    traverse(current.left)
+                if current.right != None:
+                    traverse(current.right)
+            traverse(self.root)
